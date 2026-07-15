ba06daa656c08ad15fcb2e5e179cd3572f3ad83f
aron <aronblake1010@gmail.com>
2026-07-15 22:12:32 +0800

    seckill

--- hm-dianping/src/main/resources/seckill.lua
+++ hm-dianping/src/main/resources/seckill.lua
@@ -8,6 +8,8 @@
 local voucherId = ARGV[1]
 -- 1.2用户id
 local userId = ARGV[2]
+-- 1.3订单id
+local orderId = ARGV[3]
 
 -- 2.数据key
 -- 2.1库存key
@@ -32,4 +34,6 @@ end
 redis.call('incrby',stockKey,-1)
 -- 3.5下单(保存用户)
 redis.call('sadd',orderKey,userId)
+-- 3.6发送消息到队列中
+redis.call('xadd','stream.orders','*','userId',userId,'voucherId',voucherId,'id',orderId)
 return 0
