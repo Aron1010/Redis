4a180f0e0dfd74a7b059d687b84fd3adb88e7f9e
aron <aronblake1010@gmail.com>
2026-07-21 17:13:32 +0800

    follow and feed

--- hm-dianping/src/main/java/com/hmdp/service/IFollowService.java
+++ hm-dianping/src/main/java/com/hmdp/service/IFollowService.java
@@ -1,7 +1,10 @@
 package com.hmdp.service;
 
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Follow;
 import com.baomidou.mybatisplus.extension.service.IService;
+
+
 
 /**
  * <p>
@@ -13,4 +16,9 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface IFollowService extends IService<Follow> {
 
+    Result follow(Long followUserId, boolean isFollow);
+
+    Result isFollow(Long followUserId);
+
+    Result followCommons(Long id);
 }
